3c95b786d56cd9f447210dc7893dcb6b74703beb
guerino antonini <guerantonini94@gmail.com>
2021-07-19 17:55:50 -0400

    commit tree 6eea7e240cf52bce8e211313a8f522ec3c232b27

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(acticity = "roller-skate"){
+    return `This Saturday, I want to ${acticity}!`
+}
+
+function mondayWork(acticity= "go to the office"){
+    return `This Monday, I will ${acticity}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
